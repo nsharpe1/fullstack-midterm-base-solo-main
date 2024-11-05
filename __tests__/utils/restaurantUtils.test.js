c5b38d9549411218ac0e793915f8d2e3d9b4b175
nsharpe1 <nicholas.sharpe@keyin.com>
2024-11-05 11:57:27 -0330

    Finished all working passing unit tests

--- __tests__/utils/restaurantUtils.test.js
+++ __tests__/utils/restaurantUtils.test.js
@@ -2,17 +2,30 @@ const { Cuisines, Dishes } = require("../../utils/data");
 const { generateRandomMenuItem, generateMenu, selectRandomCuisine } = require("../../utils/restaurantUtils");
 
 describe('Restaurant Functions', () => {
-    describe('generateRandomMenuItem', () => {
-      let randomitem = generateRandomMenuItem(Dishes.italian);
-      console.log(randomitem);
-      expect(Dishes.italian).toContain(randomitem);
-    });
-
-    //describe('generateMenu', () => {
-      // Test implementations go here
-    //});
-
-    //describe('selectRandomCuisine', () => {
-      // Test implementations go here
-    //});
+  describe("generateRandomMenuItem", () => {
+  test('User gets a random dish', () => {
+      const dishes = Dishes.italian;
+      const randomDish = generateRandomMenuItem(dishes);
+      expect(dishes).toContainEqual(randomDish);
+  })
+});
+  describe("generateMenu", () => {
+  test('User gets a menu', () => {
+      const italianmenu = Dishes.italian;
+      const indianmenu = Dishes.indian;
+      const chinesemenu = Dishes.chinese;
+      const veganmenu = Dishes.vegan;
+      const mexicanmenu = Dishes.mexican;
+      const menus = [italianmenu, indianmenu, chinesemenu, veganmenu, mexicanmenu];
+      const fullMenu = generateMenu(menus);
+      expect(menus).toContainEqual(fullMenu);
+  })
+});
+  describe("selectRandomCuisine", () => {
+  test('User gets a random cuisine', () => {
+      const cuisines = Cuisines;
+      const randomCuisine = selectRandomCuisine(cuisines);
+      expect(cuisines).toContainEqual(randomCuisine);
+  })
+});
 });
